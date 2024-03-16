7c8b999c3597d774ce82c9b70b9df3b64ac3a3f6
Paralleltree <paralleltree@outlook.com>
2024-03-16 20:53:19 +0900

    Add BuildIterator function

--- lib/iterator.go
+++ lib/iterator.go
@@ -1,4 +1,39 @@
 package lib
 
+// Type IteratorFunc[T any] is a function type that returns an item, a boolean indicating whether the item is valid and can try to move on to next one, and an error.
+// Caller should not process the item if the second returned value is false.'
+// ex. `item, found, err := iter()` // where `found` is false, `item` is not valid.
+type IteratorFunc[T any] func() (T, bool, error)
+
 // Type ChunkIteratorFunc[T any] is a function type that returns a chunk of items, a boolean indicating whether there are more items, and an error.
 type ChunkIteratorFunc[T any] func() ([]T, bool, error)
+
+func BuildIterator[T any](chunkIterator ChunkIteratorFunc[T]) IteratorFunc[T] {
+	var buffer []T
+	var empty T
+	hasNext := true
+	current := 0
+	return func() (T, bool, error) {
+		for {
+			// reached the end of the buffer
+			if len(buffer) <= current && hasNext {
+				newBuf, newHasNext, err := chunkIterator()
+				if err != nil {
+					return empty, false, err
+				}
+				buffer = newBuf
+				hasNext = newHasNext
+				current = 0
+			} else {
+				break
+			}
+		}
+
+		if len(buffer) <= current && !hasNext {
+			return empty, false, nil
+		}
+
+		current++
+		return buffer[current-1], current-1 < len(buffer), nil
+	}
+}
---
+++ lib/iterator_test.go
@@ -0,0 +1,94 @@
+package lib_test
+
+import (
+	"testing"
+
+	"github.com/paralleltree/markov-bot-go/lib"
+)
+
+func TestBuildIterator_IteratesOverMultipleChunks(t *testing.T) {
+	itemsPerPage := 10
+	breakAtTheLastChunkFunc := func(wantItemsCount int) func() ([]int, bool, error) {
+		head := 0
+		return func() ([]int, bool, error) {
+			buf := make([]int, 0, itemsPerPage)
+			for i := 0; i < itemsPerPage; i++ {
+				if wantItemsCount <= head+len(buf) {
+					break
+				}
+				buf = append(buf, head)
+			}
+			head += len(buf)
+			return buf, head < wantItemsCount, nil
+		}
+	}
+
+	breakAfterTheLastChunkFunc := func(wantItemsCount int) func() ([]int, bool, error) {
+		head := 0
+		return func() ([]int, bool, error) {
+			if head == wantItemsCount {
+				return nil, false, nil
+			}
+			buf := make([]int, 0, itemsPerPage)
+			for i := 0; i < itemsPerPage; i++ {
+				if wantItemsCount <= head+len(buf) {
+					break
+				}
+				buf = append(buf, head)
+			}
+			head += len(buf)
+			return buf, true, nil
+		}
+	}
+
+	cases := []struct {
+		name           string
+		wantItemsCount int
+		buildChunkFunc func(int) func() ([]int, bool, error)
+	}{
+		{
+			name:           "chunkFunc returns false as hasNext at the last chunk",
+			wantItemsCount: 15,
+			buildChunkFunc: breakAfterTheLastChunkFunc,
+		},
+		{
+			name:           "chunkFunc returns false as hasNext after the last chunk",
+			wantItemsCount: 15,
+			buildChunkFunc: breakAtTheLastChunkFunc,
+		},
+		{
+			name:           "chunkFunc returns false as hasNext at the last chunk and the number of items is multiple of itemsPerPage",
+			wantItemsCount: 20,
+			buildChunkFunc: breakAfterTheLastChunkFunc,
+		},
+		{
+			name:           "chunkFunc returns false as hasNext after the last chunk and the number of items is multiple of itemsPerPage",
+			wantItemsCount: 20,
+			buildChunkFunc: breakAtTheLastChunkFunc,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			chunkFunc := tt.buildChunkFunc(tt.wantItemsCount)
+			iter := lib.BuildIterator(chunkFunc)
+
+			gotCount := 0
+			for {
+				_, hasNext, err := iter()
+				if err != nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+				if !hasNext {
+					break
+				}
+
+				gotCount++
+			}
+
+			if tt.wantItemsCount != gotCount {
+				t.Errorf("unexpected count: want %d items, but got %d items", tt.wantItemsCount, gotCount)
+			}
+		})
+	}
+}

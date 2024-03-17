66cccb5d469e0361f719d95d231a913de575d3c1
Paralleltree <paralleltree@outlook.com>
2024-03-17 14:18:26 +0900

    Remove mastodon_config.go

--- config/mastodon_config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type MastodonConfig struct {
-	Origin         string `yaml:"origin"`
-	AccessToken    string `yaml:"access_token"`
-	PostVisibility string `yaml:"post_visibility"`
-}

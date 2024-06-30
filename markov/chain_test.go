2c418a46be62608e92cb39170b7314b33eb04d87
Paralleltree <paralleltree@outlook.com>
2024-06-30 20:23:15 +0900

    Add test for reconstructing Chain from dumped data

--- markov/chain_test.go
+++ markov/chain_test.go
@@ -1,6 +1,7 @@
 package markov_test
 
 import (
+	"reflect"
 	"slices"
 	"testing"
 
@@ -36,5 +37,28 @@ func TestChain_Generate_WithSingleSentence_Returns_Same_Sentence(t *testing.T) {
 	wantResult := []string{"A", "B", "C"}
 	if !slices.Equal(wantResult, gotResult) {
 		t.Fatalf("unexpected result: want %v, but got %v", wantResult, gotResult)
+	}
+}
+
+func TestChain_DumpAndLoad_Restores_Model(t *testing.T) {
+	// arrange
+	stateSize := 3
+	originalChain := markov.NewChain(stateSize)
+	originalChain.AddSource([]string{"A", "B", "C"})
+	originalChain.AddSource([]string{"A", "B", "Z"})
+
+	// act
+	originalChainDump, err := originalChain.Dump()
+	if err != nil {
+		t.Fatalf("unexpected error while dumping chain: %v", err)
+	}
+	restoredChain, err := markov.LoadChain(originalChainDump)
+	if err != nil {
+		t.Fatalf("unexpected error while loading chain: %v", err)
+	}
+
+	// assert
+	if !reflect.DeepEqual(originalChain, restoredChain) {
+		t.Fatalf("unexpected result: original: %v, but restored: %v", originalChain, restoredChain)
 	}
 }

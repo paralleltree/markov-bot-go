2cb8bcf9aadb95075efb69821e14d003fb166a7f
Paralleltree <paralleltree@outlook.com>
2024-10-06 03:20:02 +0000

    Add test for handler.GenerateAndPost

--- handler/post_test.go
+++ handler/post_test.go
@@ -35,3 +35,44 @@ func TestGenerateAndPost_WhenModelIsEmpty_ReturnsGenerateFailedError(t *testing.
 		t.Errorf("run() should return ErrGenerateFailed, but got: %v", err)
 	}
 }
+
+func TestGenerateAndPost_ReturnsTextFromModel(t *testing.T) {
+	cases := []struct {
+		inputText string
+	}{
+		{
+			inputText: "アルミ缶の上にあるミカン",
+		},
+		{
+			inputText: "GetPostsFetcherの呼び出し",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.inputText, func(t *testing.T) {
+			// arrange
+			ctx := context.Background()
+			postClient := blog.NewRecordableBlogClient(nil)
+			fetchClient := blog.NewRecordableBlogClient([]string{tt.inputText})
+			store := persistence.NewMemoryStore()
+
+			if err := handler.BuildChain(ctx, fetchClient, analyzer, store); err != nil {
+				t.Errorf("BuildChain() should not return error, but got: %v", err)
+			}
+
+			// act
+			err := handler.GenerateAndPost(ctx, postClient, store)
+
+			// assert
+			if err != nil {
+				t.Errorf("run() should not return error, but got: %v", err)
+			}
+			if len(postClient.PostedContents) != 1 {
+				t.Errorf("unexpected items count: want %d, but got %d", 1, len(postClient.PostedContents))
+			}
+			if tt.inputText != postClient.PostedContents[0] {
+				t.Errorf("unexpected output: want %s, but got %s", tt.inputText, postClient.PostedContents[0])
+			}
+		})
+	}
+}
